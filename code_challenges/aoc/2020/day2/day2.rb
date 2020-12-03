cc636103517ca83acdf6bb87acf87ba7acd6f170
Shawna Duvall <shawnabduvall@gmail.com>
2020-12-02 16:46:01 -0800

    day 2 complete

---
+++ code_challenges/aoc/2020/day2/day2.rb
@@ -0,0 +1,51 @@
+
+def run
+    parse_text
+    p "First result is #{validate_1(parse_text)}!"
+    p "Second result is #{validate_2(parse_text)}!"
+end
+
+def parse_text
+    f = File.open("./input.txt")
+    data = f.read.split("\n")
+end
+
+
+def validate_1(data)
+    valid = 0
+    data.each do |unit|
+        set = unit.split(" ")
+        numbers = set[0].split[0]
+        min = numbers.split("-")[0].to_i
+        max = numbers.split("-")[1].to_i
+        letter = set[1].split('')[0]
+        password = set[2]
+
+        if password.count(letter).between?(min, max)
+            valid +=1
+        end
+    end
+    valid
+end
+
+def validate_2(data)
+    valid = 0
+    data.each do |unit|
+        set = unit.split(" ")
+        numbers = set[0].split[0]
+        position1 = (numbers.split("-")[0].to_i) -1
+        position2 = (numbers.split("-")[1].to_i) -1
+        letter = set[1].split('')[0]
+        password = set[2]
+
+        unless password[position1] == password[position2]
+            if password[position1] == letter  || password[position2] == letter
+                valid +=1
+            end
+        end
+    end
+    valid
+
+end
+
+run
