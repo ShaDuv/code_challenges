63e1e2f6da516dece344e5087bfd449fa4c97247
Shawna Duvall <shawnabduvall@gmail.com>
2020-12-03 16:34:14 -0800

    part one done

--- code_challenges/aoc/2020/day3/day3.rb
+++ code_challenges/aoc/2020/day3/day3.rb
@@ -9,16 +9,24 @@ def traverse (matrix)
     tree_count = 0
     space_count = 0
     position = 0
-    matrix.each do |line|
-        line[position] == "#" ? tree_count +=1 : space_count +=1
-        if position + 3 <= line.length
+    matrix.each_with_index do |line, i|
+        if line[position] == "#"
+            tree_count +=1
+            tobaggan = "X" 
+        else 
+            space_count +=1
+            tobaggan = "O" 
+        end
+        if position + 3 <= (line.length) -1
             position += 3
         else
             position =  (line.length - (position + 3)).abs
-        end   
+        end
+        line[position] = tobaggan
+        p "#{line} : #{i}, #{position}"  
     end
-    p "Trees: #{tree_count}."
-    p "Spaces: #{space_count}."
+    p "Trees: #{tree_count}"
+    p "Spaces: #{space_count}"
 end
 
 matrix
