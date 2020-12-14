e7e80f4b29134fbca504538e28cf3ac81929647b
Shawna Duvall <shawnabduvall@gmail.com>
2020-12-14 15:28:14 -0800

    day 9 complete 'sliding window'

--- aoc/2020/day9/day9.rb
+++ aoc/2020/day9/day9.rb
@@ -18,4 +18,33 @@ def search(data)
     end
 end
 
-p search(data)
+def sliding_window (data, search)
+    target = search.to_i
+    min = 0
+    total = data[min].to_i
+    start = 0
+    finish = 0
+    
+    until total == target
+        min +=1
+        max = min
+        total = data[min].to_i
+       while total < target
+            max +=1
+           total += data[max].to_i
+        end 
+        start = min
+        finish = max
+    end
+    range = data[start, (finish - start)]
+    p range.max.to_i + range.min.to_i
+
+    # data[data[min]..data[max]]each do |n|
+    #     a.push(n)
+    # end
+    # p a
+end
+
+
+# p search(data)
+sliding_window(data, search(data))
